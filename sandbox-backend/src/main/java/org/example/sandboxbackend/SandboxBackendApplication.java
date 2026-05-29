5fe9dff155da24319b8f8975ffe75b09a871f3a1
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2026-05-29 11:31:18 +0700

    Initial commit

---
+++ sandbox-backend/src/main/java/org/example/sandboxbackend/SandboxBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.sandboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxBackendApplication.class, args);
+    }
+
+}
---
+++ sandbox-backend/src/test/java/org/example/sandboxbackend/SandboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sandboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
